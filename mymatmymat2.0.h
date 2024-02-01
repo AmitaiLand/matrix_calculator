4d107f1a79a5c32224c9b72e9c27db9a238c7966
AmitaiLand <153751095+AmitaiLand@users.noreply.github.com>
2024-02-01 17:06:52 +0200

    Create mymatmymat2.0.h

---
+++ mymatmymat2.0.h
@@ -0,0 +1,23 @@
+//
+// Created by Amitai on 1/4/2024.
+//
+
+#ifndef MATRIX_CALCULATOR_MYMAT_H
+#define MATRIX_CALCULATOR_MYMAT_H
+// h.mymat
+
+typedef struct { /*defining the matrix*/
+    double data[16];
+} mat;
+/*defining the functions*/
+void print_mat(mat *matrix);
+void add_mat(mat *result, mat *matrix_a, mat *matrix_b);
+void sub_mat(mat *result, mat *matrix_a, mat *matrix_b);
+void mul_mat(mat *matrix_a, mat *matrix_b, mat *result);
+void mul_scalar(mat *matrix, double scalar, mat *result);
+void trans_mat(mat *result, mat *matrix);
+void mat_set_element(mat *matrix, int location, double num);
+void mat_reset(mat *matrix);
+
+#endif
+
